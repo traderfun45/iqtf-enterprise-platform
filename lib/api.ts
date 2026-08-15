8548d7fe976d2075f109dfcaffd9ffd0b1ba5b52
IQTF Developer <traderfun45@gmail.com>
2026-08-15 13:52:00 +0700

    chore: lock stable dashboard baseline

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,5 @@
 const API =
-  process.env.NEXT_PUBLIC_API_URL ?? "http://127.0.0.1:4100"
+  process.env.NEXT_PUBLIC_API_URL ?? "http://127.0.0.1:4000"
 
 export async function apiGet<T>(path: string): Promise<T> {
   const response = await fetch(`${API}${path}`, {
